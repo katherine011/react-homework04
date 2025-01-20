8964fcca58db95778b9d66af10fc519afe2379c1
katherine011 <guluaq09@gmail.com>
2025-01-20 20:44:25 +0400

    done

--- src/__atoms/Line1Src/Line1Src.jsx
+++ src/__atoms/Line1Src/Line1Src.jsx
@@ -5,14 +5,13 @@ import Wfire from "../Wfire/Wfire";
 import Wleaf from "../Wleaf/Wleaf";
 
 const Line1Src = (props) => {
-  // console.log(props.wheat);
   console.log(props.wheat);
   console.log(props.fire);
   console.log(props.leaf);
   return (
     <div className="Line1Src">
-      {/* {props.wheat && <Wheat wheat={props.wheat} />}
-      {props.fire && <Wfire fire={props.fire} />} */}
+      {props.wheat && <Wheat wheat={props.wheat} />}
+      {props.fire && <Wfire fire={props.fire} />}
       {props.leaf && <Wleaf leaf={props.leaf} />}
     </div>
   );
